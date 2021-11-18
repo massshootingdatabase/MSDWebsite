04649dbc3613479cafea9661e722305a817d169f
Kevin Huang <huangk11@uci.edu>
2021-11-17 21:36:55 -0800

    created backend for email form, put values into external environment file

--- backend-server/index.js
+++ backend-server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 // Routers needed for the server...
 let incidents = require("./incidents");
+let email = require("./email");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // a Router is used to handle all endpoints from this URL.
 app.use("/api/incidents", incidents); 
+app.use("/api/email", email);
 
 app.listen(PORT, () => {
     console.log(`Mass Shooting Database listening at http://localhost:${PORT}`);
