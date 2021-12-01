1b954c4a0998adfe130b63a55b48555088439e7f
Kevin Huang <huangk11@uci.edu>
2021-12-01 13:39:45 -0800

    tweaked incidents.js

--- backend-server/incidents.js
+++ backend-server/incidents.js
@@ -17,6 +17,50 @@ let conn = mongoose.connect(process.env.DB_SERVER + process.env.DB); // create d
 // since we created default connection, use mongoose.model to use the default connection
 let incidentModel = mongoose.model("Incident", incidentSchema);
 
+// endpoint for indicidual incidents...
+router.get("/:gva_id(\\d+)", async (req, res) => {
+  // /api/incidents/19878
+  let query = await incidentModel
+    .findOne(
+      { gva_id: req.params.gva_id }, 
+      setupProject(req.query))
+    .exec();
+  res.json(query);
+});
+
+//constructs a query from an advanced search form
+router.get("/", async (req, res) => {
+  /* default search parameters are:
+    {
+      limit: 10
+      offset: 0
+      orderby: {date: descending}
+    }*/
+  let limit = checkIfNonnegativeInteger(req.query.limit, 10);
+  let offset = 0;
+
+  console.log(
+    "Query strings:",
+    req.query,
+    "Processed:",
+    setupQuery(req.query),
+    "Limit",
+    limit
+  );
+
+  //setting project because search results are not going to show everything right away
+  //adding offset for pagination purposes
+  let query = await incidentModel
+    .find(setupQuery(req.query), setupProject(req.query))
+    .sort(setupOrderBy(req.query))
+    .skip(offset)
+    .limit(limit);
+
+  res.json(query);
+});
+
+
+// HELPERS DOWN HERE
 function checkIfNonnegativeInteger(input, valueToReturn) {
   /**
    * Returns valueToReturn if input is not a string
@@ -29,81 +73,20 @@ function checkIfNonnegativeInteger(input, valueToReturn) {
   return integer;
 }
 
-router.get("/:gva_id(\\d+)", async (req, res) => {
-  // /api/incidents/19878
-  let query = await incidentModel.findOne({ gva_id: req.params.gva_id }).exec();
-  res.json(query);
-});
-
-/*
-router.get("/", async (req, res) => {
-    let limit = checkIfNonnegativeInteger(req.query.limit, 100);
-    console.log("Query strings:", req.query, "Processed:", setupFind(req.query),  "Limit", limit);
-    let query = await incidentModel.find(setupFind(req.query)).sort({date: "descending"}).limit(limit);
-    res.json(query);
-});
-*/
-
-//constructs a query from an advanced search form
-router.get("/", async (req, res) => {
-  /* default search parameters are:
-    {
-      limit: 10
-      offset: 0
-      orderby: {date: descending}
-    }*/
-  let limit = checkIfNonnegativeInteger(req.query.limit, 10);
-  let offset = 0;
-  let direction =
-    req.query.direction !== undefined ? req.query.direction : "descending";
-
-  let orderBy;
-  switch (req.query.order) {
-    case "deaths":
-      orderBy = { deaths: direction };
-    default:
-      orderBy = { date: direction };
-  }
-
-  console.log(
-    "Query strings:",
-    req.query,
-    "Processed:",
-    setupQuery(req.query),
-    "Limit",
-    limit
-  );
-
-  //setting filter because search results are not going to show everything right away
-  //adding offset for pagination purposes
-  let query = await incidentModel
-    .find(setupQuery(req.query), {
-      gva_id: 1,
-      incident_name: 1,
-      city: 1,
-      state: 1,
-      deaths: 1,
-      wounded: 1,
-      start_date: 1,
-    })
-    .sort(orderBy)
-    .skip(offset)
-    .limit(limit);
-
-  res.json(query);
-});
 
 function setupQuery(queryStrings) {
-  /* Takes the query strings and creates the arguments for find */
+  /**
+   * Parses the query strings object and returns findOptions object
+   * containing the appropriate arugments for find
+   */
   let findOptions = {};
-  // process fields
+  
+  // a query string param must be defined to handle it
+  // process city and state
   if (typeof queryStrings.state === "string") {
     findOptions.state = queryStrings.state;
   }
-  if (
-    queryStrings.city !== undefined &&
-    typeof queryStrings.city === "string"
-  ) {
+  if (typeof queryStrings.city === "string") {
     findOptions.city = queryStrings.city;
   }
 
@@ -115,7 +98,6 @@ function setupQuery(queryStrings) {
       Number.MAX_SAFE_INTEGER
     ),
   };
-
   // process injured
   findOptions.wounded = {
     $gte: checkIfNonnegativeInteger(queryStrings.minWounded, 0),
@@ -126,6 +108,41 @@ function setupQuery(queryStrings) {
   };
 
   return findOptions;
+}
+
+function setupProject(queryStrings) {
+  /**
+   * Returns an filterOptions object that dictates which elements to return
+   * /api/incidents?project=gva_id,incident_name,city,state
+   */
+  if (queryStrings.project === "string") {
+    let projectOptions = {};
+    queryStrings.project.split(",").forEach(element => {projectOptions[element] = 1;});
+    return projectOptions;
+  } else {
+    return {
+      gva_id: 1,
+      incident_name: 1,
+      city: 1,
+      state: 1,
+      deaths: 1,
+      wounded: 1,
+      start_date: 1,
+    };
+  }
+}
+
+function setupOrderBy(queryStrings) {
+  /**
+   * Returns an orderBy object that dictates what order the elements sorted by
+   * order of strings matter!
+   * api/incidents?orderBy=gva_id1,city0
+   */
+  let orderBy = {};
+  queryStrings.orderBy
+    .split(",")
+    .forEach(element => {element[element.length - 1] === 1 ? projectOptions[element] = "asc" : "desc";});
+  return orderBy;
 }
 
 module.exports = router;
