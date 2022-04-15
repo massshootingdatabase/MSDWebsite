80861d4751ccc3fa4070e568584b7fff8e34ccbb
codemanrj <rmontano11@student.mtsac.edu>
2022-04-15 15:02:06 -0700

    added logic for emailing, implemented forgot password endpoint

--- backend-server/controllers/auth.js
+++ backend-server/controllers/auth.js
@@ -1,6 +1,8 @@
 const Admin = require("../models/Admin");
+const User = require("../models/User");
 
 const ErrorResponse = require("../utils/errorResponse");
+const sendEmail = require("../utils/sendEmail");
 
 exports.register = async (req, res, next) => {
     const {email, password} = req.body;
@@ -46,31 +48,49 @@ exports.login = async (req, res, next) => {
 
 exports.forgotpassword = async (req, res, next) => {
     const {email} = req.body;
-    try {
-        const admin = await Admin.findOne({email});
 
-        if(!admin) {
-            return
-             next(new ErrorResponse("Email could not be sent", 404));
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+        return next(new ErrorResponse("No email could not be sent", 404));
         }
 
-        const resetToken = admin.getResetPasswordToken();
+        // Reset Token Gen and add to database hashed (private) version of token
+        const resetToken = user.getResetPasswordToken();
 
-        await admin.save();
-        
+        await user.save();
+
+        // Create reset url to email to provided email
         const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
 
+        // HTML Message
         const message = `
-            <h1> You have requested a password reset </h1>
-            <p> Please go to this link to reset your password. </p>
-            <a href=${resetUrl} clicktracking=off> ${resetUrl} </a>
-        `
+        <h1>You have requested a password reset</h1>
+        <p>Please make a put request to the following link:</p>
+        <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+        `;
 
-        //TODO
-        //set up emailing using node mailer and sendgrid
-        
+        try {
+            await sendEmail({
+                to: user.email,
+                subject: "Password Reset Request",
+                text: message,
+            });
+
+            res.status(200).json({ success: true, data: "Email Sent" });
+        } catch (err) {
+            console.log(err);
+
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+
+            await user.save();
+
+            return next(new ErrorResponse("Email could not be sent", 500));
+        }
     } catch(error) {
-        
+        next(error);
     }
     
 };
