094e584d5bb955de546308c871f1489e2070411d
codemanrj <rmontano11@student.mtsac.edu>
2022-05-21 19:28:38 -0700

    deleted data.js files, queries for incidents while reside in incidents.js -- added the old query code

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -57,6 +57,13 @@ const addVictim = async (v, incident, next) => {
     //console.log(incident);
 }
 
+
+
+/*-----------------------------------------------------*/
+/*------------------- DATA QUERYING -------------------*/
+/*-----------------------------------------------------*/
+
+
 //get the incident with the specified id
 exports.get = async (req, res, next) => {
     const {id} = req.body;
@@ -78,7 +85,163 @@ exports.get = async (req, res, next) => {
     }
 };
 
+/* Might replace the above function with this one
+router.get("/:gva_id(\\d+)", async (req, res) => {
+  // /api/incidents/19878
+  let query = await incidentModel
+    .findOne(
+      { gva_id: req.params.gva_id }, 
+      setupProject(req.query))
+    .exec();
+  res.json(query);
+});
+*/
 
+//constructs a query from an advanced search form
+router.get("/", async (req, res) => {
+  /* default search parameters are:
+    {
+      limit: 10
+      offset: 0
+      orderby: {date: descending}
+    }*/
+  let limit = parseInt(req.query.limit);
+  if (isNaN(limit)) {
+    limit = 10;
+  }
+  let offset = parseInt(req.query.offset);
+  if (isNaN(offset)) {
+    offset = 0;
+  }
+
+  //setting project because search results are not going to show everything right away
+  //adding offset for pagination purposes
+  let query = incidentModel
+    .find(setupQuery(req.query), setupProject(req.query))
+    .sort(setupOrderBy(req.query))
+    .skip(offset)
+    .limit(limit);
+  
+  console.log(query.getFilter(), query.getOptions(), query.projection());
+
+  let results = await query.exec();
+  res.json(results);
+});
+
+
+
+function createRange(min, max) {
+  let options = {};
+  let minInt = parseInt(min);
+  let maxInt = parseInt(max);
+
+  if (!isNaN(minInt)) {
+    options.$gte = minInt;
+  }
+
+  if (!isNaN(maxInt)) {
+    options.$lte = maxInt;
+  }
+
+  return options;
+}
+
+
+function setupQuery(queryStrings) {
+  /**
+   * Parses the query strings object and returns findOptions object
+   * containing the appropriate arugments for find
+   */
+  let findOptions = {};
+  
+  // checking if the type of a query string is a string
+  // is a quick way to check if the query string param has a usable value
+  let params = ["state", "city", "postal_code", "congressional", "stateSenate", "stateHouse", "place_type"]
+  params.forEach(element => {
+    if (typeof queryStrings[element] === "string") {
+      findOptions[element] = queryStrings[element];
+    }
+  });
+
+  // process deaths
+  let deathRange = createRange(queryStrings.minKilled, queryStrings.maxKilled);
+  // object.keys is the way to get the length of an object
+  // we can't pass undefined, null, nan, or empty objects to an param
+  //  otherwise search fails. we only want to pass an object when we want
+  //  to do a ranged search (a min and/or max)
+  if (Object.keys(deathRange).length > 0) {
+    findOptions.deaths = deathRange;
+  }
+  // process wounded
+  let woundedRange = createRange(queryStrings.minWounded, queryStrings.maxWounded);
+  if (Object.keys(woundedRange).length > 0) {
+    findOptions.wounded = woundedRange;
+  }
+  
+  // process date
+  let dateRange = {};
+  // the createRange function doesn't work with dates...
+  if (typeof queryStrings.before === "string") {
+    dateRange.$lte = queryStrings.before;
+  }
+  if (typeof queryStrings.after === "string") {
+    dateRange.$gte = queryStrings.after;
+  }
+  if (Object.keys(dateRange).length > 0) {
+    findOptions.date = dateRange;
+  }
+
+  return findOptions;
+}
+
+function setupProject(queryStrings) {
+  /**
+   * Returns an filterOptions object that dictates which elements to return
+   * /api/incidents?include=gva_id,incident_name,city,state&exclude=_id
+   */
+ 
+  let projectOptions = {};
+
+  if (typeof queryStrings.include === "string") {
+    queryStrings.include.split(",").forEach(element => {projectOptions[element] = 1;});
+  };
+
+  if (typeof queryStrings.exclude === "string") {
+    queryStrings.exclude.split(",").forEach(element => {projectOptions[element] = 0;});
+  };
+
+  // this could only happen if include and exclude isn't provided
+  if (projectOptions.length === 0) {
+    projectOptions = {
+      _id: 1,
+      gva_id: 1,
+      incident_name: 1,
+      city: 1,
+      state: 1,
+      deaths: 1,
+      wounded: 1,
+      start_date: 1,
+    };
+  }
+    
+    return projectOptions;
+  
+}
+
+function setupOrderBy(queryStrings) {
+  /**
+   * Returns an orderBy object that dictates what order the elements sorted by
+   * order of strings matter!
+   * api/incidents?orderBy=gva_id1,city0
+   */
+  let orderBy = {};
+  if (typeof queryStrings.orderBy === "string") {
+    queryStrings.orderBy
+      .split(",")
+      .forEach(element => {element[element.length - 1] === 1 ? orderBy[element] = "asc" : "desc";});
+  }
+  return orderBy;
+}
 
 
 /*
