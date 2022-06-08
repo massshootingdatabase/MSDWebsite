d81be6cb8074e37fb20d9162ef655ba31fd6c2fa
codemanrj <rmontano11@student.mtsac.edu>
2022-06-08 00:05:12 -0700

    add endOfLine: auto to prettier. completed linting for backend. email.js

--- email.js
+++ email.js
@@ -1,4 +1,4 @@
-/* This defines a router handling all requests to api/email/
+// This defines a router handling all requests to api/email/
 // This is done to keep index.js light
 
 // libraries needed in general
@@ -6,45 +6,43 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 
-let router = express.Router();
+const router = express.Router();
 // assume form data is sent as application/x-www-form-urlencoded
 // we pick this since multipart isn't necessary
-router.use(express.urlencoded({extended: false}));
+router.use(express.urlencoded({ extended: false }));
 
-router.post("/", (req, res) => {    
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        // WILL NEED TO REPLACE WITH LEGIT SMTP SERVER
-        auth: {
-            user: process.env.TEST_EMAIL_USER,
-            pass: process.env.TEST_EMAIL_PASS
-        }
-    });
-    // tests whether transporter is made
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log("Error");
-        } else {
-            console.log("Success! Server ready to send...");
-        }
-    })
-    // set up the email content
-    let email_data = {
-            from: req.body.from, // req.body.from
-            to: process.env.OUR_EMAIL,
-            subject: req.body.subject, // req.body.subject
-            text: req.body.message // req.body.text
-    };
-    transporter.sendMail(email_data, (err, info) => {
-        if (err) {
-            console.log(error);
-        } else {
-            console.log(info);
-        }
-    });
+router.post("/", (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    // WILL NEED TO REPLACE WITH LEGIT SMTP SERVER
+    auth: {
+      user: process.env.TEST_EMAIL_USER,
+      pass: process.env.TEST_EMAIL_PASS,
+    },
+  });
+  // tests whether transporter is made
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log("Error");
+    } else {
+      console.log("Success! Server ready to send...");
+    }
+  });
+  // set up the email content
+  const emailData = {
+    from: req.body.from, // req.body.from
+    to: process.env.OUR_EMAIL,
+    subject: req.body.subject, // req.body.subject
+    text: req.body.message, // req.body.text
+  };
+  transporter.sendMail(emailData, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
 });
 
-
 module.exports = router;
-*/
