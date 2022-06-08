dc6eb4bb5e774587147132faf52eda7d3533f9b0
codemanrj <rmontano11@student.mtsac.edu>
2022-06-07 23:12:20 -0700

    ran eslinting on all /routes files

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {signup, confirm, upload, del} = require('../controllers/newsletter');
+const { signup, confirm } = require("../controllers/newsletter");
 
-router.route('/signup').post(signup);
-router.route('/confirm').get(confirm);
-//router.route('/upload').post(upload);
-//router.route('/delete').get(del);
+router.route("/signup").post(signup);
+router.route("/confirm").get(confirm);
+// router.route('/upload').post(upload);
+// router.route('/delete').get(del);
 
 module.exports = router;
-
-
--- routes/private.test.js
+++ routes/private.test.js
@@ -1,11 +1,10 @@
-const app = require('../index') // Link to your server file
-const supertest = require('supertest')
-const request = supertest(app)
-const mongoose = require('mongoose');
-const Admin = require('../models/Admin');
+const app = require("../index"); // Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
+const mongoose = require("mongoose");
+const Admin = require("../models/Admin");
 
-describe('insert', () => {
-
+describe("insert", () => {
   beforeAll(async () => {
     await mongoose.connect(process.env.DB_SERVER);
   });
@@ -14,22 +13,21 @@ describe('insert', () => {
     await mongoose.connection.close();
   });
 
-  it('should return unauthorized request', async() => {
+  it("should return unauthorized request", async () => {
     const response = await request.get("/api/private");
     expect(response.statusCode).toBe(401);
-    expect(response.body.error).toBe("Not authorized to access this route")
+    expect(response.body.error).toBe("Not authorized to access this route");
   });
 
-  it('should return private data', async() => {
-    const admin = await Admin.findOne({email: "superuser@gmail.com"});
+  it("should return private data", async () => {
+    const admin = await Admin.findOne({ email: "superuser@gmail.com" });
     const token = admin.getSignedToken();
 
     const auth = "Bearer " + token;
 
-    const response1 = await request.get("/api/private").set('Authorization', auth);
+    const response1 = await request
+      .get("/api/private")
+      .set("Authorization", auth);
     expect(response1.statusCode).toBe(200);
   });
-
 });
-
-
